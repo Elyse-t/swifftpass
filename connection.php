4181399d07222e18d4f9270b9163ea3fa5c1a945
Elyse <elysetuyishimire50@gmail.com>
2025-10-23 22:29:30 +0200

    remote db

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";   // usually localhost
-$username   = "root";        // default in XAMPP
-$password   = "";            // default in XAMPP is empty
-$dbname     = "swifftpass"; // change to your DB name
+$servername = "bpoejafa5q9xhrvibkx6-mysql.services.clever-cloud.com";   // usually localhost
+$username   = "uzcnxswewezhsisp";        // default in XAMPP
+$password   = "kyZ8YkOVHtMQUv4Lf5hi";            // default in XAMPP is empty
+$dbname     = "bpoejafa5q9xhrvibkx6"; // change to your DB name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
